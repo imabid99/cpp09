5cfb85ddd738a1765fc5b93c782a8015544042bd
Imad Abid <imabid@student.1337.ma>
2023-04-06 15:30:27 +0000

    cpp09

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 11:23:48 by imabid            #+#    #+#             */
-/*   Updated: 2023/04/04 17:51:17 by imabid           ###   ########.fr       */
+/*   Updated: 2023/04/06 15:27:24 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,8 +71,19 @@ int     BitcoinExchange::check_date(int year, int month, int day)
         return 0;
     else if(day < 1 || day > 31)
         return 0;
-    else if(month == 2 && day > 28)
-        return 0;
+    else if(month == 2)
+    {
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            if(day > 29)
+                return 0;
+        }
+        else
+        {
+            if(day > 28)
+                return 0;
+        }
+    }
     else if((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
         return 0;
     return 1;
